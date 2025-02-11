d4d002da0411be03913f7850a6846956a381b69e
46ra20 <rakib2046.md@gmail.com>
2025-02-11 08:15:29 +0600

    Course model Add

--- Teacher/models.py
+++ Teacher/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class CourseMaterialsModel(models.Model):
+    file = models.FileField(upload_to="Media/Materials")
+    upload_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'File upload by {self.upload_by.first_name} {self.upload_by.last_name}'
+
+class CourseModel(models.Model):
+    course_title = models.CharField(max_length=200,default="")
+    materials = models.ManyToManyField(CourseMaterialsModel)
+    description = models.TextField()
+    course_credit = models.IntegerField(default=0)
+    course_created_by = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Course created by {self.course_created_by.first_name} {self.course_created_by.last_name}'
