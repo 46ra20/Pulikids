cae37a45ad9aafb0e80ee2c729b066a7d0fa0d79
46ra20 <rakib2046.md@gmail.com>
2025-02-11 07:41:17 +0600

    Add Teacher search function

--- Teacher/urls.py
+++ Teacher/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views import SearchTeacherAndStudent
 urlpatterns = [
-    
+    path('search/<name>/',SearchTeacherAndStudent.as_view({'get':'list'}),name="search")
 ]
--- Teacher/views.py
+++ Teacher/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializer import SearchTeacherAndStudentSerializer
+from rest_framework.response import Response
+from django.db.models import Q
 # Create your views here.
+
+
+# def SearchTeacherAndStudent(request,name):
+#         print(name)
+#         query = User.objects.filter(first_name = name)
+#         return HttpResponse("query")
+
+
+class SearchTeacherAndStudent(viewsets.ViewSet):
+    def list(self,request,name):
+        query = User.objects.filter(Q(first_name__icontains = name) | Q(last_name__icontains = name))
+        serializer = SearchTeacherAndStudentSerializer(query,many=True)
+        return Response(serializer.data)
+
+
